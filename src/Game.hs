aabdd8cadfdb6e746c7e54de8c3dfa1ae78523b6
Sandman1705 <sandman1705@gmail.com>
2017-12-21 15:37:34 +0100

    Drawing player and walls

--- src/Game.hs
+++ src/Game.hs
@@ -18,30 +18,29 @@ window = InWindow "SpaceShooter" (width, height) (offset, offset)
 background :: Color
 background = black
 
+
 -- | Data describing the state of the game. 
 data GameState = Game
-  { 
-  -- player :: (Float, Float)   -- player coordinates
+  { player :: (Float, Float)   -- player coordinates
   -- enemies :: [Enemy]         -- list of enemies
   -- obstcales :: [Obstacle]    -- list of obstcales
   -- playerRockets :: [Rockets] -- list of rocket fired
   -- enemyRockets :: [Rockets]  -- list of rocket fired
   -- ....
   -- timeFromLastAddedEnemy :: Float 
-  paused :: Bool
+  , paused :: Bool
   } deriving Show
     
 -- | The starting state for the game.
 initialState :: GameState
 initialState = Game
-  { 
-  -- player = (0,(-150))
+  { player = (0,(-150))
   -- enemies = []
   -- obstacle = []
   -- playerRockets = []
   -- enemiesRockets = []
   -- timeFromLastAddedEnemy = 0
-  paused = False
+  , paused = False
   }
                     
 
@@ -51,10 +50,27 @@ initialState = Game
 render :: GameState  -- ^ The game state to render.
        -> Picture   -- ^ A picture of this game state.
 render game =
-  pictures []
+  pictures [walls,spaceship]
+  where
+    -- The player
+    (px,py) = player game
+    
+    spaceship :: Picture
+    spaceship = 
+      translate px py $
+        color blue $ 
+          polygon [(0,25),(15,(-15)),(0,0),((-15),(-15)),(0,25)]
 
+    --  The bottom and top walls.
+    wall :: Float -> Picture
+    wall offset =
+      translate offset 0 $
+        color wallColor $
+          rectangleSolid 10 580
 
-
+    wallColor = greyN 0.5
+    walls = pictures [wall 185, wall (-185)]
+    
 
 
 -- | Update the game
