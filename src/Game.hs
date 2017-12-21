7d05d31d1329bee4d88954bbf5dc30470a14b435
Sandman1705 <sandman1705@gmail.com>
2017-12-21 20:23:35 +0100

    Added Rocket (draw, fire, inBounds, delete)

--- src/Game.hs
+++ src/Game.hs
@@ -24,13 +24,13 @@ data PlayerState = Player
   { pPosition :: (Float, Float) -- player coordinates
   , pSpeed :: Float             -- player movement speed
   } deriving Show              -- TODO: debug output
-  
+
 data Rocket = Rocket
   { rPosition :: (Float, Float) -- rocket coodrinates
   , rSpeed :: (Float, Float)    -- speed vector
   } deriving Show              -- TODO: debug output
-  
- 
+
+
 -- | Data describing the state of the game. 
 data GameState = Game
   { player :: PlayerState       -- state of the player
@@ -43,7 +43,7 @@ data GameState = Game
   , keysPressed :: Set Key      -- keeps track of all keys currently held down
   , paused :: Bool              -- shows if the game is currently paused
   } deriving Show               -- TODO: debug output
-    
+
 -- | The starting state for the game.
 initialState :: GameState
 initialState = Game
@@ -56,19 +56,26 @@ initialState = Game
   , keysPressed = empty
   , paused = False
   }
-                    
 
 
+-- | Produces a Picture of a given Rocket
+drawRocket :: Rocket -> Picture
+drawRocket rocket =
+  translate rx ry $
+    color yellow $
+      circle 5
+    where
+      (rx,ry) = rPosition rocket
 
 -- | Convert a game state into a picture.
 render :: GameState  -- ^ The game state to render.
        -> Picture   -- ^ A picture of this game state.
 render game =
-  pictures [walls,spaceship]
+  pictures [walls,spaceship,rockets]
   where
     -- The player
     (px,py) = pPosition $ player game
-    
+
     spaceship :: Picture
     spaceship = 
       translate px py $
@@ -84,12 +91,16 @@ render game =
 
     wallColor = greyN 0.5
     walls = pictures [wall 185, wall (-185)]
-    
-    -- TODO: Draw rockets
-    
-    
-    
-    
+
+    -- Player rockets
+    rockets :: Picture
+    rockets = pictures allRockets
+
+    allRockets :: [Picture]
+    allRockets = Prelude.map drawRocket (playerRockets game)
+
+
+
 -- | Update the player
 updatePlayer :: Set Key -> Float -> PlayerState -> PlayerState
 updatePlayer keysPressed seconds player =
@@ -109,7 +120,7 @@ updatePlayer keysPressed seconds player =
           (nx - seconds * pSpeed player)
         else 
           nx
-      
+
       ny' :: Float
       ny' = 
         if (member (Char 'w') keysPressed) 
@@ -122,12 +133,12 @@ updatePlayer keysPressed seconds player =
           (ny - seconds * pSpeed player)
         else 
           ny
-          
+
 -- | Wrapper: Calls updatePlayer for GameState
 updatePlayerInGame :: Float -> GameState -> GameState
 updatePlayerInGame seconds game =
   game { player = updatePlayer (keysPressed game) seconds (player game) }
-  
+
 -- | Update a rocket
 updateRocket :: Float -> Rocket -> Rocket
 updateRocket seconds rocket =
@@ -145,17 +156,55 @@ updatePlayerRocketsInGame seconds game =
     where
       updateAllRockets :: Float -> [Rocket] -> [Rocket]
       updateAllRockets seconds lstR = Prelude.map (updateRocket seconds) lstR 
-      
+
 -- | Adds new rocket when fired
 addRocket :: (Float,Float) -> (Float,Float) -> [Rocket] -> [Rocket]
 addRocket (px,py) (sx,sy) rockets = (Rocket (px,py) (sx,sy)) : rockets
 
- 
+-- | Player fired a rocket
+rocketFiredByPlayer :: GameState -> GameState
+rocketFiredByPlayer game = 
+  if ( member (SpecialKey KeySpace) (keysPressed game) )
+  then
+    game { playerRockets = addRocket (pPosition (player game)) (0,100) (playerRockets game) -- FIX: Remove constants for speed from here
+         , keysPressed = delete (SpecialKey KeySpace) (keysPressed game) 
+         }
+  else
+    game
+    
+-- | Checks if a Rocket has exited the screen
+rocketInBounds :: Rocket -> Bool
+rocketInBounds rocket =
+  if ( (ry > yLimit   ) || 
+       (ry < (-yLimit)) ||
+       (rx > xLimit   ) ||
+       (rx < (-xLimit))
+     )
+  then
+    False
+  else
+    True
+  where
+    (rx,ry) = rPosition rocket
+    yLimit = fromIntegral height / 2 - 20  -- FIX: (-20) is for visual test
+    xLimit = fromIntegral width / 2 - 20   -- FIX: (-20) is for visual test
+
+
+deleteOutOfBoundsRockets :: [Rocket] -> [Rocket]
+deleteOutOfBoundsRockets rockets = Prelude.filter rocketInBounds rockets
+
+deleteRocketsFormGame :: GameState -> GameState
+deleteRocketsFormGame game =
+  game { playerRockets = deleteOutOfBoundsRockets (playerRockets game) }
+
+
 -- | Update the game
 update :: Float -> GameState -> GameState 
 update seconds game = if not (paused game) 
                       then 
+                        rocketFiredByPlayer .
                         updatePlayerRocketsInGame seconds . 
+                        deleteRocketsFormGame .
                         updatePlayerInGame seconds $ 
                         game
                       else 
@@ -184,9 +233,9 @@ handleKeys (EventKey key Down _ _) game =
 -- Forget all keys not pressed anymore
 handleKeys (EventKey key Up _ _) game =
   game { keysPressed = delete key (keysPressed game) }
-  
 
-      
+
+
 -- Do nothing for all other events.
 handleKeys _ game = game
 
